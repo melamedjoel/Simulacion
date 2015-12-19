58d0a8ff59686d46faf5856024e6a7390722384f
melamedjoel <melamedjoel@gmail.com>
2015-12-19 18:26:06 -0300

    first commit

---
+++ Simulacion/Simulacion/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Simulacion
+{
+    partial class Simulacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCantEmp = new System.Windows.Forms.Label();
+            this.txtCantEmpl = new System.Windows.Forms.TextBox();
+            this.txtCantEmplNuevos = new System.Windows.Forms.TextBox();
+            this.lblCantEmplNuevos = new System.Windows.Forms.Label();
+            this.cmbTurnos = new System.Windows.Forms.ComboBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lblPPSInfo = new System.Windows.Forms.Label();
+            this.lblPECInfo = new System.Windows.Forms.Label();
+            this.lblPArrInfo = new System.Windows.Forms.Label();
+            this.lblPTO = new System.Windows.Forms.Label();
+            this.lblPArr = new System.Windows.Forms.Label();
+            this.lblPEC = new System.Windows.Forms.Label();
+            this.lblPPS = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCantEmp
+            // 
+            this.lblCantEmp.AutoSize = true;
+            this.lblCantEmp.Location = new System.Drawing.Point(54, 68);
+            this.lblCantEmp.Name = "lblCantEmp";
+            this.lblCantEmp.Size = new System.Drawing.Size(103, 13);
+            this.lblCantEmp.TabIndex = 0;
+            this.lblCantEmp.Text = "Cantidad empleados";
+            this.lblCantEmp.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtCantEmpl
+            // 
+            this.txtCantEmpl.Location = new System.Drawing.Point(205, 65);
+            this.txtCantEmpl.Name = "txtCantEmpl";
+            this.txtCantEmpl.Size = new System.Drawing.Size(100, 20);
+            this.txtCantEmpl.TabIndex = 1;
+            // 
+            // txtCantEmplNuevos
+            // 
+            this.txtCantEmplNuevos.Location = new System.Drawing.Point(205, 103);
+            this.txtCantEmplNuevos.Name = "txtCantEmplNuevos";
+            this.txtCantEmplNuevos.Size = new System.Drawing.Size(100, 20);
+            this.txtCantEmplNuevos.TabIndex = 3;
+            // 
+            // lblCantEmplNuevos
+            // 
+            this.lblCantEmplNuevos.AutoSize = true;
+            this.lblCantEmplNuevos.Location = new System.Drawing.Point(54, 106);
+            this.lblCantEmplNuevos.Name = "lblCantEmplNuevos";
+            this.lblCantEmplNuevos.Size = new System.Drawing.Size(141, 13);
+            this.lblCantEmplNuevos.TabIndex = 2;
+            this.lblCantEmplNuevos.Text = "Cantidad empleados nuevos";
+            this.lblCantEmplNuevos.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // cmbTurnos
+            // 
+            this.cmbTurnos.FormattingEnabled = true;
+            this.cmbTurnos.Items.AddRange(new object[] {
+            "Turno Mañana",
+            "Turno Tarde"});
+            this.cmbTurnos.Location = new System.Drawing.Point(362, 85);
+            this.cmbTurnos.Name = "cmbTurnos";
+            this.cmbTurnos.Size = new System.Drawing.Size(195, 21);
+            this.cmbTurnos.TabIndex = 4;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(225, 159);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(173, 23);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Correr simulacion";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lblPPSInfo
+            // 
+            this.lblPPSInfo.AutoSize = true;
+            this.lblPPSInfo.Location = new System.Drawing.Point(54, 286);
+            this.lblPPSInfo.Name = "lblPPSInfo";
+            this.lblPPSInfo.Size = new System.Drawing.Size(28, 13);
+            this.lblPPSInfo.TabIndex = 6;
+            this.lblPPSInfo.Text = "PPS";
+            this.lblPPSInfo.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // lblPECInfo
+            // 
+            this.lblPECInfo.AutoSize = true;
+            this.lblPECInfo.Location = new System.Drawing.Point(54, 316);
+            this.lblPECInfo.Name = "lblPECInfo";
+            this.lblPECInfo.Size = new System.Drawing.Size(28, 13);
+            this.lblPECInfo.TabIndex = 7;
+            this.lblPECInfo.Text = "PEC";
+            // 
+            // lblPArrInfo
+            // 
+            this.lblPArrInfo.AutoSize = true;
+            this.lblPArrInfo.Location = new System.Drawing.Point(54, 345);
+            this.lblPArrInfo.Name = "lblPArrInfo";
+            this.lblPArrInfo.Size = new System.Drawing.Size(27, 13);
+            this.lblPArrInfo.TabIndex = 8;
+            this.lblPArrInfo.Text = "PArr";
+            // 
+            // lblPTO
+            // 
+            this.lblPTO.Location = new System.Drawing.Point(390, 286);
+            this.lblPTO.Name = "lblPTO";
+            this.lblPTO.Size = new System.Drawing.Size(167, 86);
+            this.lblPTO.TabIndex = 9;
+            // 
+            // lblPArr
+            // 
+            this.lblPArr.AutoSize = true;
+            this.lblPArr.Location = new System.Drawing.Point(129, 345);
+            this.lblPArr.Name = "lblPArr";
+            this.lblPArr.Size = new System.Drawing.Size(27, 13);
+            this.lblPArr.TabIndex = 12;
+            this.lblPArr.Text = "PArr";
+            // 
+            // lblPEC
+            // 
+            this.lblPEC.AutoSize = true;
+            this.lblPEC.Location = new System.Drawing.Point(129, 316);
+            this.lblPEC.Name = "lblPEC";
+            this.lblPEC.Size = new System.Drawing.Size(28, 13);
+            this.lblPEC.TabIndex = 11;
+            this.lblPEC.Text = "PEC";
+            // 
+            // lblPPS
+            // 
+            this.lblPPS.AutoSize = true;
+            this.lblPPS.Location = new System.Drawing.Point(129, 286);
+            this.lblPPS.Name = "lblPPS";
+            this.lblPPS.Size = new System.Drawing.Size(28, 13);
+            this.lblPPS.TabIndex = 10;
+            this.lblPPS.Text = "PPS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(319, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "PTO(i)";
+            // 
+            // Simulacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 412);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPArr);
+            this.Controls.Add(this.lblPEC);
+            this.Controls.Add(this.lblPPS);
+            this.Controls.Add(this.lblPTO);
+            this.Controls.Add(this.lblPArrInfo);
+            this.Controls.Add(this.lblPECInfo);
+            this.Controls.Add(this.lblPPSInfo);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.cmbTurnos);
+            this.Controls.Add(this.txtCantEmplNuevos);
+            this.Controls.Add(this.lblCantEmplNuevos);
+            this.Controls.Add(this.txtCantEmpl);
+            this.Controls.Add(this.lblCantEmp);
+            this.Name = "Simulacion";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCantEmp;
+        private System.Windows.Forms.TextBox txtCantEmpl;
+        private System.Windows.Forms.TextBox txtCantEmplNuevos;
+        private System.Windows.Forms.Label lblCantEmplNuevos;
+        private System.Windows.Forms.ComboBox cmbTurnos;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label lblPPSInfo;
+        private System.Windows.Forms.Label lblPECInfo;
+        private System.Windows.Forms.Label lblPArrInfo;
+        private System.Windows.Forms.Label lblPTO;
+        private System.Windows.Forms.Label lblPArr;
+        private System.Windows.Forms.Label lblPEC;
+        private System.Windows.Forms.Label lblPPS;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Simulacion/Simulacion/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Random;
+using log4net;
+
+namespace Simulacion
+{
+    public partial class Simulacion : Form
+    {
+        double HV = double.MaxValue;
+        double T, TPLL, NS = 0;
+        double TF = 0;
+        double IA = 0;
+        List<double> TPS = new List<double>();
+        List<double> TA = new List<double>();
+        List<double> STO = new List<double>();
+        List<double> ITO = new List<double>();
+
+        int NT = 0;
+        double SS, SLL, STA = 0;
+        int SPA=0;
+        public Simulacion()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            lblPPS.Text = "";
+            lblPEC.Text = "";
+            lblPArr.Text ="";
+            lblPTO.Text = "";
+            T=0;
+            TPLL= 0;
+            NS = 0;
+            TF = 0;
+            IA = 0;
+            TPS = new List<double>();
+            TA = new List<double>();
+            STO = new List<double>();
+            ITO = new List<double>();
+
+            NT = 0;
+            SS = 0;
+            SLL = 0;
+            STA = 0;
+            SPA = 0;
+
+            int N = Convert.ToInt32(txtCantEmpl.Text);
+            int M = Convert.ToInt32(txtCantEmplNuevos.Text);
+            for (int i=0; i < (Convert.ToInt32(txtCantEmpl.Text) + Convert.ToInt32(txtCantEmplNuevos.Text)); i++) {
+                TPS.Add(HV);
+                STO.Add(0);
+                ITO.Add(0);
+                TA.Add(0);
+            }
+
+            if (cmbTurnos.Text == "Turno Mañana")
+            {
+                T = 28800; //8 de la mañana en segundos
+                TPLL = T;
+                TF = 50400; //14 de la tarde en segundos
+            }
+            else {
+                T = 50400;
+                TPLL = T;
+                TF = 72000; //20 de la noche en segundos
+            }
+
+            while (T <= TF || (T > TF && NS>0)) {
+                int menorTPS = buscarMenorSalida(N, M);
+                
+                if (TPS[menorTPS] < TPLL)
+                {
+                    realizarSalida(menorTPS, N, M);
+                }
+                else
+                {
+                    realizarLlegada(N, M);
+                }
+
+                if (T > TF) {
+                    TPLL = HV; 
+                }
+
+            }
+
+            imprimirResultados(N, M);
+
+   
+
+        }
+
+        private void imprimirResultados(int N, int M)
+        {
+            lblPPS.Text = (Math.Round((SS - SLL) / NT,2)).ToString();
+            lblPEC.Text = (Math.Round((SS - SLL - STA) / NT, 2)).ToString();
+            lblPArr.Text = ((SPA*100) / (NT+SPA)).ToString();
+            for (int i=0; i < TPS.Count; i++) {
+                lblPTO.Text += "Puesto nro " + (i+1) + ", PTO: " + Math.Round((STO[i]*100/T),2).ToString()+ "\n\r";
+            }
+            MessageBox.Show(NT.ToString());
+
+        }
+
+        private void realizarLlegada(int N, int M)
+        {
+            T = TPLL;
+            IA = calcularIA();
+            TPLL = T + IA;
+            bool Arr = RealizarArrepentimiento();
+
+            if (Arr == false) {
+                NS = NS + 1;
+                SLL = SLL + T;
+                if (NS <= N + M) {
+                    int puesto = buscarPuesto();
+                    STO[puesto] = STO[puesto] + (T - ITO[puesto]);
+                    TA[puesto] = calcularTA(M);
+                    TPS[puesto] = T + TA[puesto];
+                    STA = STA + TA[puesto];
+                }
+            }
+
+        }
+
+        private int buscarPuesto()
+        {
+            int posicionPuestoHV = TPS.IndexOf(TPS.Find(x => x == HV));
+            int mayorOcioso = posicionPuestoHV;
+            for (int j = 0; j < TPS.Count; j++) {
+
+                if (TPS[j] == HV) {
+                    if (STO[j] > STO[posicionPuestoHV])
+                    {
+                        mayorOcioso = j;
+                    }
+                }
+            }
+            return mayorOcioso;
+
+        }
+
+        private bool RealizarArrepentimiento()
+        {
+            if ((NS - 1) > 3) {
+                double R = dameRandom();
+                if (R < 40) {
+                    SPA++;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private double calcularIA()
+        {
+            double R = dameRandom();
+            if (cmbTurnos.Text == "Turno Tarde")
+            {
+                return new LogNormal(4.9074, 1.0703).Sample();
+                //return new Gamma(1.1804, 179).InverseCumulativeDistribution(R/100);
+            }
+            else {
+                return new LogNormal(4.9788, 1.0368).Sample();
+                //return new LogNormal(4.9788, 1.0368).InverseCumulativeDistribution(R/100);
+            }
+        }
+
+        private void realizarSalida(int menorTPS, int N, int M)
+        {
+            T = TPS[menorTPS];
+            NS = NS - 1;
+            if (NS >= N + M)
+            {
+                TA[menorTPS] = calcularTA(M);
+                TPS[menorTPS] = T + TA[menorTPS];
+                STA = STA + TA[menorTPS];
+            }
+            else {
+                ITO[menorTPS] = T;
+                TPS[menorTPS] = HV;
+            }
+
+            SS = SS + T;
+            NT = NT + 1;
+
+        }
+
+        private double calcularTA(int M)
+        {
+            double OS = 25;
+            double R = dameRandom();
+
+            if (R <= OS)
+            {
+                //OBRA SOCIAL
+                return new Normal(323.19, 183.3).Sample() + M*60;
+                //return new Normal(323.19, 183.3).InverseCumulativeDistribution(R/100) + M * 60;
+            }
+            else { 
+                //PARTICULAR
+                return new Weibull(1.5565, 100.47).Sample() + M * 60;
+                //return new Gamma(1.7458, 54.354).InverseCumulativeDistribution(R/100) + M * 60;
+            }
+        }
+
+        private static double dameRandom()
+        {
+            Random rnd = new Random();
+            double R = rnd.Next(1, 101);
+            return R;
+        }
+
+        private int buscarMenorSalida(int N, int M)
+        {
+            return TPS.IndexOf(TPS.Min());
+        }
+
+    }
+}
